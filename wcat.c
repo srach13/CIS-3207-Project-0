e99132084f2908fe927251723192ec0bbf591101
Sameera Rachakonda <srachakonda6@gmail.com>
2021-01-25 13:54:48 -0500

    Main function

--- wcat.c
+++ wcat.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 64 //SIZE is the max size of buffer string used by fgets
 
+//main function
+int main(int argc, char* argv[]) {
+    if(argc==1) {
+        exit(0);
+    }
+    char buffer[SIZE];          //buffer string for reading from file
+}
 int i;
 for(i=1; i<argc; i++) {
     FILE *fp = fopen(argv[i], "r");
     if(fp==NULL) {
         printf("cannot open file\n");
-        exit(1);
+        exit(0);
     }
+    while(fgets(buffer, SIZE, fp)!=NULL) {
+        printf("%s",buffer);
+    }
+    fclose(fp);
 }
+return 0;
+
 
