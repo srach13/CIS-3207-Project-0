045ece072a589e5f21970289837893461b1159e3
Sameera Rachakonda <srachakonda6@gmail.com>
2021-01-25 14:19:16 -0500

    Searching for word

--- wgrep.c
+++ wgrep.c
@@ -4,5 +4,18 @@
 #define MAX_SIZE 1024   //max size of input string when argc==2 or buffer when argc>2
 
 int main(int argc, char*argv[]) {
-
+    if(argc==1) {
+        exit(1);
+    }
+    else if(argc==2) {  //number of arguments is 2 -> use argv[1] as search
+        char *search = argv[1];
+        while (1) {
+            char str[MAX_SIZE]; //input string of defined size MAX_SIZE
+            scanf("%s", str);   //read string from standard input
+            char *found = strstr(str, search);      //search given search variable in standard input
+            if (found != NULL) {                     //returns NULL if it doesn't find string
+                printf("%s\n", str);                //if it finds string then it prints the input string
+            }
+        }
+    }
 }
