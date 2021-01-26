feb6c8a4d23a3a5e6a279c78afea2a2178fe085f
Sameera Rachakonda <srachakonda6@gmail.com>
2021-01-26 13:54:16 -0500

    Error case with one argument, case with two arguments and reading from keyboard

--- wgrep.c
+++ wgrep.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv) {
-    char *filename=NULL, *searchTerm;
-    FILE *fp;
+int main(int argc, char* argv[]) {
     char line[1024];
-    int isFile=0;
-
-    if(argc<2){     //case where it is passed no arguments
-        printf("grep:searchterm filename\n");
-        return 1;
+    if(argc == 1) {     //case where there's 1 argument
+        exit(1);
+    } else if(argc == 2) {  //case with 2 arguments makes argv[1] searchTerm
+        char* searchTerm = argv[1];
+        while(!feof(stdin)) {   //read input until terminated by user
+            fgets(line,sizeof(line),stdin);
+            line[strlen(line)-1]='\0';
+            if(strstr(line,searchTerm)) {
+                printf("%s\n",line);
+            }
+        }
     }
-    else if(argc<3) {
-        searchTerm=(char*)malloc(strlen(argv[argc-1])+1);
-        strcpy(searchTerm,argv[argc-1]);
-    }
-
 }
-
-
-
