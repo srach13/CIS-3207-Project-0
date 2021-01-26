a88adbeac3ad187802fa4ae59fb4c12e4f5a37fb
Sameera Rachakonda <srachakonda6@gmail.com>
2021-01-26 14:07:01 -0500

    Comments

--- wcat.c
+++ wcat.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 64 //SIZE is the max size of buffer string used by fgets
+#define MAX_SIZE 64
 
-//main function
 int main(int argc, char* argv[]) {
-    if (argc==1) {
+    if (argc == 1) {    //no files specified error
         exit(0);
     }
-    char buffer[SIZE];          //buffer string for reading from file
+    char buffer[SIZE];  //buffer string for reading from file
     int i;
-    for (i=1; i<argc; i++) {                 //iterate
-        FILE *fp = fopen(argv[i], "r");     //argv[i] is filename
-        if (fp==NULL) {
-            printf("cannot open file\n");
-            exit(0);                        //error if file is NULL
+    for (i=1; i<argc; i++) {                       //for loop to iterate
+        FILE *fp = fopen(argv[i], "r");
+        if (fp==NULL) {                            //error message for NULL file
+            printf("Cannot open file\n");
+            exit(0);
         }
-        while (fgets(buffer, SIZE, fp) !=
-               NULL) {   //reads from file in buffer, if it returns NULL then there is no content to read from file
+        while (fgets(buffer, SIZE, fp) != NULL) { //reads from file in buffer
             printf("%s", buffer);                //prints content of file to standard output
         }
-        printf("\n"); //new line to separate different files
         fclose(fp);
     }
     return 0;
